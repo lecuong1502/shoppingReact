98783bb4e138adbe69af78c98d51bcbe64a98260
lecuong1502 <kiencuongle1442@gmail.com>
2024-04-16 15:57:40 +0700

    detail product

--- src/screens/ProductDetailScreen/index.js
+++ src/screens/ProductDetailScreen/index.js
@@ -1,9 +1,45 @@
-import './index.css';
+import "./index.css";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { BASE_URL } from "../../environment";
 
 const ProductDetailScreen = () => {
-    return(
-        <div>LoginScreen</div>
-    )
-}
+  const [product, setProduct] = useState([]);
+  const navigate = useNavigate();
+  const { id } = useParams();
+  useEffect(() => {
+    try {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      fetch(`${BASE_URL}/api/products/${id}`, requestOptions)
+        .then((response) => response.text())
+        .then(async (result) => {
+          const resultJson = JSON.parse(result);
+          if (resultJson?.id) {
+            setProduct(resultJson);
+          }
+        })
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  return (
+    <div>
+          <div>name: {product?.productName}</div>
+          <div>image: {product?.image}</div>
+          <div>desc: {product?.description}</div>
+          <div>price: {product?.a_unit_of_price}</div>
+    </div>
+  );
+};
 
 export default ProductDetailScreen;
--- src/screens/ProductsScreen/index.js
+++ src/screens/ProductsScreen/index.js
@@ -1,47 +1,52 @@
-import './index.css';
+import "./index.css";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from "../../environment";
 
 const ProductsScreen = () => {
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    try {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
 
-    const [products, setProducts] = useState([]);
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
 
-    useEffect(() => {
-       try {
-         var myHeaders = new Headers();
-         myHeaders.append("Content-Type", "application/json");
+      fetch(`${BASE_URL}/api/products`, requestOptions)
+        .then((response) => response.text())
+        .then(async (result) => {
+          const resultJson = JSON.parse(result);
+          if (resultJson?.data) {
+            setProducts(resultJson?.data);
+          }
+        })
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
 
-         var requestOptions = {
-           method: "GET",
-           headers: myHeaders,
-           redirect: "follow",
-         };
-
-         fetch(`${BASE_URL}/api/products`, requestOptions)
-           .then((response) => response.text())
-           .then(async (result) => {
-             const resultJson = JSON.parse(result);
-             if (resultJson?.data) {
-               setProducts(resultJson?.data);
-             }
-           })
-           .catch((error) => console.log("error", error));
-       } catch (error) {
-         console.log(error);
-       }
-    }, []);
-
-    return (
-      <div>
-        {products?.map((item) => (
-          <div>
-            <div>name: {item?.productName}</div>
-            <div>desc: {item?.description}</div>
-          </div>
-        ))}
-      </div>
-    );
-}
+  return (
+    <div>
+      {products?.map((item) => (
+        <div
+          onClick={() => {
+            navigate(`/products/${item?.id}`);
+          }}
+        >
+          <div>name: {item?.productName}</div>
+          <div>image: {item?.image}</div>
+          <div>desc: {item?.description}</div>
+          <div>price: {item?.a_unit_of_price}</div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ProductsScreen;
