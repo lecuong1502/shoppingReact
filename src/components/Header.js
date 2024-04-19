d515b14272571586c2c1a9fdff1a9f5a01e50b71
Thang Le <tleduc@pentalog.com>
2024-04-19 23:19:12 +0700

    check login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,40 @@
 import React, { useState, useEffect } from "react";
 import "./Header.css";
 import logo from "../assets/ES-SHOP.png";
-import TextField from '@mui/material/TextField';
-import 
+import TextField from "@mui/material/TextField";
 
 function Header() {
-    return (
-        <div className="header">
-            <div className="logo">
-                <img src={logo} width={100} height={100}></img>
-            </div>
-            <div className="search">
-                <TextField
-                    variant="outlined"
-                    fullWidth
-                    label="Search"
-                />
-            </div>
-            <div className="profile">
-                <div className="login">
-                    <button id="login" ></button>
-                </div>
-            </div>
-        </div>
-    );
-}
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-async function CheckLogin() {
+  const checkLogin = async () => {
     const token = await localStorage.getItem("ACCESS_TOKEN");
     if (token) {
-        return true;
+      setIsLoggedIn(true);
     }
-    return false;
+    setIsLoggedIn(false);
+  }
+
+  useEffect(() => {
+    checkLogin();
+  }, []);
+
+  return (
+    <div className="header">
+      <div className="logo">
+        <img src={logo} width={100} height={100}></img>
+      </div>
+      <div className="search">
+        <TextField variant="outlined" fullWidth label="Search" />
+      </div>
+      <div className="profile">
+        {isLoggedIn ? (
+            <div>logged in</div>
+        ) : (
+            <div>not logged in</div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Header;
