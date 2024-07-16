a3114ceefb40d7d4b8ad99099542d250b341a9fa
lecuong1502 <kiencuongle1442@gmail.com>
2024-07-16 12:00:32 +0700

    order

--- src/screens/OrderList/index.js
+++ src/screens/OrderList/index.js
@@ -5,14 +5,55 @@ import { BASE_URL } from "../../environment";
 import Header from "../../components/Header";
 
 const OrderList = () => {
-    
+  const [products1, setProducts1] = useState([]);
+  const [products2, setProducts2] = useState([]);
+  useEffect(() => {
+    getProducts();
+  }, []);
 
-    return(
+  const getProducts = async () => {
+    try {
+      const token = await localStorage.getItem("ACCESS_TOKEN");
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("token", token);
+
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      fetch(`${BASE_URL}/api/history-order`, requestOptions)
+        .then((response) => response.text())
+        .then(async (result) => {
+          const resultJson = JSON.parse(result);
+          if (resultJson?.data) {
+            setProducts1(resultJson?.data.userData);
+            setProducts2(resultJson?.data.productData);
+          }
+        })
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <Header />
+      {products1?.map((item) => (
         <div>
-            <Header />
-            <div>123</div>
+          <div>User name: {item.name}</div>
         </div>
-    );
+      ))}
+      {products2?.map((item) => (
+        <div>
+          <div>Product Name: {item.productName}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default OrderList;
